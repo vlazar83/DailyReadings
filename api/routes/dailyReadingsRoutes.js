ef7327e977fca949604241485e5d615ab328bfb1
admin <lazar.viktor@gmail.com>
2021-04-10 19:57:01 +0200

    merge route files into one to successful swagger generation

--- api/routes/dailyReadingsRoutes.js
+++ api/routes/dailyReadingsRoutes.js
@@ -14,6 +14,36 @@ const jsonKeyUsersAuthonfig = JSON.parse(JSON.stringify(keyUsersAuthConfig))
 
 const basicAuth = require('express-basic-auth');
 
+router.get('/dailyReadings',(req, res) => {
+  /*  #swagger.tags = ['dailyReadings']
+      #swagger.description = 'Endpoint to get the dailyReadings. Returns readings which were checked by the admin before.' */
+
+      /* #swagger.responses[200] = { 
+          schema: { "$ref": "#/definitions/dailyReadingsOutput" },
+          description: "Returning data." } */
+
+  dailyReadings.find({status: ["checked"]}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+});
+
+router.get('/dailyReading/:year/:month/:day', (req, res) => {
+  /*  #swagger.tags = ['dailyReadings']
+        #swagger.description = 'Endpoint to get the dailyReading.' */
+  
+    /* #swagger.responses[200] = { 
+            schema: { "$ref": "#/definitions/dailyReading" },
+            description: "Returning data." } */
+
+  dailyReadings.findOne({ readingYear: req.params.year, readingMonth: req.params.month, readingDay: req.params.day },function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  })
+});
+
 router.post('/dailyReading',basicAuth(jsonKeyUsersAuthonfig),(req, res) => {
 
       /*  #swagger.tags = ['dailyReadings']
