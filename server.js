8ff4ec9f3e70d41d1551989061665fbf1a106dc1
admin <lazar.viktor@gmail.com>
2021-04-11 13:38:17 +0200

    CORS support

--- server.js
+++ server.js
@@ -36,6 +36,26 @@ app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
+// CORS support
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://lebeny-lutheran.hu/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 //app.listen(port);
 const server = https.createServer({key: key, cert: cert , secureProtocol: 'TLSv1_2_method'}, app);
